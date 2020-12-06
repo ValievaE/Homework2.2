d9900ffd75fbcdd512a9b384bfbb5dcecf3ed4d3
Elena Valieva <valieva-1991@mail.ru>
2020-12-06 15:27:37 +0300

    homework 2.2 06/12/2020

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,19 @@
+fun main() {
+    var likes = 11
+
+    val a = likes % 10
+
+    println(likes)
+
+    if (a == 1 && !(likes.toString().endsWith("11"))) {
+        println(" cheloveku")
+    } else {
+        println(" ludyam")
+    }
+
+
+}
+
+
+
+
